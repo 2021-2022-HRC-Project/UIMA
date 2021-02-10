fffe1b628f4c3f198b9e20d1c33bfc097f7529ec
Xusheng Liu <liuxs981001@gmail.com>
2021-02-09 23:42:59 -0500

    Update NLP_Processor_String in Utils file to correct the NLP processor output

--- NLPAnnotator/src/main/java/CoreNLP/utils/Utils.java
+++ NLPAnnotator/src/main/java/CoreNLP/utils/Utils.java
@@ -8,7 +8,7 @@ import java.util.Set;
 public class Utils {
     public final static List<String> directionSet = Arrays.asList("up", "down", "left", "right", "on", "between", "north", "south", "east", "west", "under", "in_front_of", "behind", "in", "on_top_of", "from", "onto");
     public final static String NOT_FOUND = "???";
-    public final static String NLP_PROCESSOR_STRING = "NLPProcessor";
+    public final static String NLP_PROCESSOR_STRING = "edu.rosehulman.aixprize.pipeline.types.NLPProcessor";
 
     /**
      * Helper function; check whether an string is a direction word
