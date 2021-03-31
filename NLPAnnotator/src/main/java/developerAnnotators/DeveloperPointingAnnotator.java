1f01fa4e3ea48e9a1b3d1ce38811acdf7091f59d
Xusheng Liu <liuxs981001@gmail.com>
2021-03-30 20:10:16 -0400

    Update developer data

--- NLPAnnotator/src/main/java/developerAnnotators/DeveloperPointingAnnotator.java
+++ NLPAnnotator/src/main/java/developerAnnotators/DeveloperPointingAnnotator.java
@@ -9,7 +9,7 @@ public class DeveloperPointingAnnotator extends Annotator {
 
     @Override
     public String process(String request) throws IOException {
-        String result = "[{'id':1.0,'confidence':0.7083620230684211},{'id':2.0,'confidence':0.24950724697827417},{'id':3.0,'confidence':0.08218268979262786},{'id':4.0,'confidence':-0.07900363845721572},{'id':5.0,'confidence':-0.5846593124985601},{'id':6.0,'confidence':0.06710809200111145},{'id':7.0,'confidence':-0.022121716926781342},{'id':8.0,'confidence':-0.9156020968540153},{'id':9.0,'confidence':-0.5315046072402078},{'id':10.0,'confidence':-0.8471382144358836},{'id':11.0,'confidence':-0.6316842563953068}]";
+        String result = "[{'sofa':1,'begin':0,'end':0,'id':1.0,'confidence':0.7883012291917103},{'sofa':1,'begin':0,'end':0,'id':2.0,'confidence':0.4192664971408399},{'sofa':1,'begin':0,'end':0,'id':3.0,'confidence':0.7654227001515287},{'sofa':1,'begin':0,'end':0,'id':4.0,'confidence':0.9984557463704878},{'sofa':1,'begin':0,'end':0,'id':5.0,'confidence':-0.8508741601239602}]";
         return "{"+unitWrapper+result+"}";
     }
 }
